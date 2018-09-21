5ce3548d63431547b3c73d9f5f147d449f70370c
Land Lu <v-luting@microsoft.com>
2018-09-21 16:01:30 +0800

    Add project files.

---
+++ ZXingScannerDemo/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using Foundation;
+using System;
+using System.Diagnostics;
+using UIKit;
+using ZXing.Mobile;
+
+namespace ZXingScannerDemo
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void BtnClick(UIKit.UIButton sender)
+        {
+            MobileBarcodeScanner scanner = new MobileBarcodeScanner();
+            ZXingScannerViewController scannerViewController = new ZXingScannerViewController(MobileBarcodeScanningOptions.Default, scanner);
+            NavigationController.PushViewController(scannerViewController, true);
+
+            scannerViewController.OnScannedResult += (result) =>
+            {
+                InvokeOnMainThread(() =>
+                {
+                    NavigationController.PopViewController(true);
+                    Debug.WriteLine("Scanned Barcode: " + result.Text);
+                });
+                
+            };           
+        }
+
+        async partial void PresentBtnClick(UIKit.UIButton sender)
+        {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan();
+
+            if (result != null)
+                Console.WriteLine("Scanned Barcode: " + result.Text);
+        }
+    }
+}
---
+++ ZXingScannerDemo/ViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ZXingScannerDemo
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Action ("BtnClick:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnClick (UIKit.UIButton sender);
+
+        [Action ("PresentBtnClick:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PresentBtnClick (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
